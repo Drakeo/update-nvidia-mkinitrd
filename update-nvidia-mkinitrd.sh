b1382448fb289b2494106ed4e16a1212ebddb979
drakeo <flats_fixed@flatsfixedbicycles.com>
2017-07-27 23:37:00 -0500

    fix partition type

--- update-nvidia-mkinitrd.sh
+++ update-nvidia-mkinitrd.sh
@@ -91,5 +91,5 @@ mount|grep ' / '|cut -d' ' -f 1 >> drive.txt
 DRIVE=$(sed "s/\(.\{9\}\).\{6\}//" "${DRTXT}")
 rm  $DRTXT
 
-mkinitrd -c -k $KERNV -f $EXT -r $DRIVE -m xhci-pci:ohci-pci:ehci-pci:xhci-hcd:uhci-hcd:ehci-hcd:hid:usbhid:i2c-hid:hid_generic:hid-cherry:hid-logitech:hid-logitech-dj:hid-logitech-hidpp:hid-lenovo:hid-microsoft:hid_multitouch:jbd2:mbcache:ext4 -u -o /boot/initrd.gz
+mkinitrd -c -k $KERNV -f $EXT -r $DRIVE -m xhci-pci:ohci-pci:ehci-pci:xhci-hcd:uhci-hcd:ehci-hcd:hid:usbhid:i2c-hid:hid_generic:hid-cherry:hid-logitech:hid-logitech-dj:hid-logitech-hidpp:hid-lenovo:hid-microsoft:hid_multitouch:jbd2:mbcache:$EXT  -u -o /boot/initrd.gz
 #clean up 
