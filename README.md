c68d69935e9d28de8e2fff42e08eebd5b20f066e
drakeo <flats_fixed@flatsfixedbicycles.com>
2017-07-27 21:31:48 -0500

    first push

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # update-nvidia-mkinitrd
+~~~
+Place the update-nvidia-mkinitrd 
+/usr/local/bin or where you like. 
+As root in runlevel 3 or init 3
+type update-nvidia-mkinitrd.sh
+use this to upgrade the nvidia driver after a kernel upgrade
+will also build the initrd image of the new installed kernel.
+~~~
+
+# edit your partition file extention 
+~~~
+#edit for my system
+EXT=ext4 #to what your drive is formated to. 
+~~~
+
+# Not for legacy drivers
+~~~
+Downloads the "latest.txt"
+reads it and downloads the kernel for your arch.
+It reads your new /boot/config file.
+
+Uninstalls the old NVidia driver 
+Then builds the NVidia driver that is 
+installed. Since it reads the config. 
+It builds the driver for it.
+
+If you have upgraded you linux kernel it 
+will read  the new /boot/config
+and build the initrd.gz for your drive.
+~~~
+# Note on the mkinitrd
+
+~~~
+you can edit it to your system use
+Alien bob's /usr/share/mkinitrd/mkinitrd_command_generator.sh
+then edit your system. As you may want.
+This is for a generic sytstem.
+It will find your /dev/root and the kernel modules installed.
+~~~
+For my use and ease. 
---
+++ update-nvidia-mkinitrd.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+# Init
+FILE="/tmp/out.$$"
+GREP="/bin/grep"
+#....
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root if you install regular user
+   please use the ./install.sh command " 1>&2
+   exit 1
+fi
+
+# ...
+   
+ if [ -z "$ARCH" ]; then
+  case "$( uname -m )" in
+    i?86) ARCH=i486 ;;
+    arm*) ARCH=arm ;;
+       *) ARCH=$( uname -m ) ;;
+  esac
+fi
+
+
+if [ "$ARCH" = "i586" ]; then
+  SLKCA="Linux-x86"
+  KERNH="-smp"
+elif [ "$ARCH" = "i686" ]; then
+   SLKCA="Linux-x86"
+   KERNH="-smp"
+elif [ "$ARCH" = "x86_64" ]; then
+   SLKCA="Linux-x86_64"
+   KERNH=""
+else
+   SLKCA="Linux-x86_64"
+fi
+
+cd /root
+
+filename=latest.txt
+
+wget  https://download.nvidia.com/XFree86/$SLKCA/$filename
+
+#edit for my system
+EXT=ext4
+GETNV=$(sed "s/\(.\{7\}\).\{0\}//" "${filename}")
+RUNNV=$(sed "s/\(.\{14\}\).\{0\}//" "${filename}")
+
+echo down loading $RUNNV
+wget -c  https://download.nvidia.com/XFree86/$SLKCA/$GETNV
+rm  $filename
+#get installed kernel version
+KOUT=kernelv.txt
+sed -n 3p /boot/config >> koutput.txt
+awk '{print($3)}' koutput.txt >>  kernelv.txt
+
+KERNV=$(sed "s/\(.\{9\}\).\{6\}//" "${KOUT}")
+#uninstall NVIDIA you are running.
+
+# ...
+function pause(){
+   read -p "$*"
+}
+
+echo -e "\e[1;33m NVidia driver upgrade For kernel installed.\e[0m"
+pause 'Press [Enter] key to continue or ctrl c to stop...'
+#clean up 
+rm koutput.txt
+rm kernelv.txt
+
+sh $RUNNV --uninstall
+
+#install NVIDIA kernel version for kernel upgrades
+sh $RUNNV -k $KERNV $KERNH
+
+# ...
+function pause(){
+   read -p "$*"
+}
+
+echo -e "\e[1;33m Make intrid image for new kernel or press ctrl c to stop.\e[0m"
+pause 'Press [Enter] key to continue or ctrl c to stop...'
+# rest of the script
+#
+
+#remove old intrid tree
+rm -rf /boot/initrd*
+#get root partition 
+DRTXT=drive.txt
+mount|grep ' / '|cut -d' ' -f 1 >> drive.txt
+
+DRIVE=$(sed "s/\(.\{9\}\).\{6\}//" "${DRTXT}")
+rm  $DRTXT
+
+mkinitrd -c -k $KERNV -f $EXT -r $DRIVE -m xhci-pci:ohci-pci:ehci-pci:xhci-hcd:uhci-hcd:ehci-hcd:hid:usbhid:i2c-hid:hid_generic:hid-cherry:hid-logitech:hid-logitech-dj:hid-logitech-hidpp:hid-lenovo:hid-microsoft:hid_multitouch:jbd2:mbcache:ext4 -u -o /boot/initrd.gz
+#clean up 
