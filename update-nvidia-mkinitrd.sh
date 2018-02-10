3c9ea5de0115e6c05b61b75bca1a5d53790373c8
drakeo <flats_fixed@flatsfixedbicycles.com>
2018-02-10 12:40:57 -0600

    find partition format set for 2 or 3 digit nvidia and kernel

--- update-nvidia-mkinitrd.sh
+++ update-nvidia-mkinitrd.sh
@@ -33,27 +33,43 @@ elif [ "$ARCH" = "x86_64" ]; then
 else
    SLKCA="Linux-x86_64"
 fi
+#cd usr/local/bin/update-nvidia-mkinitrd
+echo check for updates
+#git pull
 
 cd /root
 
+# This is nividia's latest stable
 filename=latest.txt
 
 wget  https://download.nvidia.com/XFree86/$SLKCA/$filename
 
-#edit for my system
-EXT=ext4
+# 2 digit ending for nvidia 
 GETNV=$(sed "s/\(.\{7\}\).\{0\}//" "${filename}")
 RUNNV=$(sed "s/\(.\{14\}\).\{0\}//" "${filename}")
+# 3 digit ending for nvidia 
+GETNV2=$(sed "s/\(.\{8\}\).\{0\}//" "${filename}")
+RUNNV2=$(sed "s/\(.\{16\}\).\{0\}//" "${filename}")
 
-echo down loading $RUNNV
+
+#wget -c  https://download.nvidia.com/XFree86/$SLKCA/$GETNV
+URL=https://download.nvidia.com/XFree86/$SLKCA/$GETNV
+if wget --spider $URL 2>/dev/null; then
 wget -c  https://download.nvidia.com/XFree86/$SLKCA/$GETNV
+BRNV=$RUNNV
+else
+wget -c  https://download.nvidia.com/XFree86/$SLKCA/$GETNV2
+BRNV=$RUNNV2
+fi
+echo down loading $BRNV
+#remove for rerun
 rm  $filename
 #get installed kernel version
 KOUT=kernelv.txt
 sed -n 3p /boot/config >> koutput.txt
 awk '{print($3)}' koutput.txt >>  kernelv.txt
 
-KERNV=$(sed "s/\(.\{9\}\).\{6\}//" "${KOUT}")
+KERNV=$(sed "s/\(.\{10\}\).\{6\}//" "${KOUT}")
 #uninstall NVIDIA you are running.
 
 # ...
@@ -67,10 +83,10 @@ pause 'Press [Enter] key to continue or ctrl c to stop...'
 rm koutput.txt
 rm kernelv.txt
 
-sh $RUNNV --uninstall
+sh $BRNV --uninstall
 
 #install NVIDIA kernel version for kernel upgrades
-sh $RUNNV -k $KERNV$KERNH
+sh $BRNV -k $KERNV$KERNH
 
 # ...
 function pause(){
@@ -84,12 +100,17 @@ pause 'Press [Enter] key to continue or ctrl c to stop...'
 
 #remove old intrid tree
 rm -rf /boot/initrd*
-#get root partition 
+#set drive partition and partition extension
 DRTXT=drive.txt
-mount|grep ' / '|cut -d' ' -f 1 >> drive.txt
-
+DTEXT=exttype.txt
+mount|grep ' / '|cut -d' ' -f 1 >> drive.txt 
 DRIVE=$(sed "s/\(.\{9\}\).\{6\}//" "${DRTXT}")
+blkid -o export $DRIVE | grep '^TYPE' | cut -d"=" -f2 >> exttype.txt
+EXT=$(sed "s/\(.\{9\}\).\{6\}//" "${DTEXT}")
+# clean up  for rerun
 rm  $DRTXT
+rm  $DTEXT
+#build the initrd.img
 echo -e "\e[1;33m mkinitrd -c -k $KERNV$KERNH -f $EXT -r $DRIVE -m xhci-pci:ohci-pci:ehci-pci:xhci-hcd:uhci-hcd:ehci-hcd:hid:usbhid:i2c-hid:hid_generic:hid-cherry:hid-logitech:hid-logitech-dj:hid-logitech-hidpp:hid-lenovo:hid-microsoft:hid_multitouch:jbd2:mbcache:$EXT  -u -o /boot/initrd.gz \e[0m" 
 echo -e "\e[1;33m wait 15 seconds so you can make sure the drive and ext is right hit ctrl c if not \e[0m"
 sleep 15
